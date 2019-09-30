d7e46067a2e6b137c2a3f1c146427d06f6df3bb0
Mary Kanu <maryikanu@gmail.com>
2019-09-30 02:40:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 
-first_number = 4
-second_number = 4
+first_number = 8
+second_number = 6
 
 sum = first_number + second_number
 difference = first_number - second_number
