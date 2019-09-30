a1f10b7c76ab208e40d1da0378dde3c97983f576
Mary Kanu <maryikanu@gmail.com>
2019-09-30 02:38:36 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -4,8 +4,11 @@ it "contains a local variable called first_number that is assigned to a number" 
   first_number = get_variable_from_file('./calculator.rb', "first_number")
  
   expect(first_number).to be_an(Integer).or be_a(Float)
+
+second_number = get_variable_from_file('./calculator.rb', "second_number")
+
+expect(second_number). to be_an(Integer). or be_a(Float)
+
 end
 
-first_number = get_variable_from_file('.calculator.rb',"first_number")
 
-expect(second_number).not_to equal(0)
